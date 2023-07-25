fe66130524df1959f667a8ced9018a57963ccb67
Joy Samaddar <joysamaddar123@gmail.com>
2023-07-25 07:11:52 +0530

    Project boilerplate code cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        secondary: ['var(--font-secondary)'],
       },
+      colors: {
+        black: "var(--black)",
+        white: "var(--white)",
+        gray: "var(--gray)",
+      }
     },
   },
   plugins: [],
