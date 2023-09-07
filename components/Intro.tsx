56df2de9fc4042c4cc4c699a71cfa525353d2f64
Joy Samaddar <joysamaddar123@gmail.com>
2023-09-07 08:39:57 +0530

    REFACTOR: Intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,51 +1,39 @@
-"use client"
+"use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
+import { useScroll } from "framer-motion";
 import { useRef } from "react";
 import Heading from "./ui/Heading";
+import { RevealingTextContainer, RevealingTextItem } from "./ui/RevealingText";
 
 export default function Intro() {
   const sectionRef = useRef<HTMLElement>(null);
   const { scrollYProgress } = useScroll({
     target: sectionRef,
-    offset: ["start end", "end start"]
+    offset: ["start end", "end start"],
   } as any);
-  const width1 = useTransform(scrollYProgress, [0.35, 0.5], ["100%", "0%"])
-  const width2 = useTransform(scrollYProgress, [0.35, 0.6], ["100%", "0%"])
-  const width3 = useTransform(scrollYProgress, [0.35, 0.7], ["100%", "0%"])
-  const width4 = useTransform(scrollYProgress, [0.35, 0.8], ["100%", "0%"])
-  const width5 = useTransform(scrollYProgress, [0.35, 0.9], ["100%", "0%"])
-  const width6 = useTransform(scrollYProgress, [0.35, 1], ["100%", "0%"])
+  const texts = [
+    "Grew up fiddling with",
+    "paintbrushes and computers,",
+    "and the artistic and analytical side",
+    "in me has since transitioned into",
+    "developing pixel-perfect UIs",
+    "and robust backends.",
+  ];
 
   return (
-    <section id="about" ref={sectionRef} className="relative w-screen px-[8%] pt-[12rem] pb-[6rem] select-none flex flex-col">
+    <section
+      id="about"
+      ref={sectionRef}
+      className="relative w-screen px-[8%] pt-[12rem] pb-[6rem] select-none flex flex-col"
+    >
       <Heading>ABOUT ME</Heading>
-      <div className="flex flex-col gap-6 mt-12 font-semibold text-6xl text-white">
-        <div className="relative">
-          <p>Grew up fiddling with</p>
-          <motion.div style={{width: width1}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-        <div className="relative">
-          <p>paintbrushes and computers,</p>
-          <motion.div style={{width: width2}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-        <div className="relative">
-          <p>and the artistic and analytical side</p>
-          <motion.div style={{width: width3}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-        <div className="relative">
-          <p>in me has since transitioned into</p>
-          <motion.div style={{width: width4}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-        <div className="relative">
-          <p>developing pixel-perfect UIs</p>
-          <motion.div style={{width: width5}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-        <div className="relative">
-          <p>and robust backends.</p>
-          <motion.div style={{width: width6}} className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"></motion.div>
-        </div>
-      </div>
+      <RevealingTextContainer scrollYProgress={scrollYProgress}>
+        {texts.map((text, i) => (
+          <RevealingTextItem index={i} key={i}>
+            {text}
+          </RevealingTextItem>
+        ))}
+      </RevealingTextContainer>
     </section>
   );
 }
---
+++ components/ui/RevealingText.tsx
@@ -0,0 +1,64 @@
+import { MotionValue, motion, useTransform } from "framer-motion";
+import React from "react";
+
+interface RevealingTextContainerProps {
+  scrollYProgress: MotionValue<number>;
+  widthStart?: number;
+  widthEnd?: number;
+  children: JSX.Element[];
+}
+
+export function RevealingTextContainer({
+  scrollYProgress,
+  widthStart = 0.35,
+  widthEnd = 0.5,
+  children,
+}: RevealingTextContainerProps) {
+  const widthDiff = (1 - widthEnd) / (children.length - 1);
+
+  return (
+    <div className="flex flex-col gap-6 mt-12 font-semibold text-6xl text-white">
+      {children.map((child) => {
+        return React.cloneElement(child, {
+          scrollYProgress,
+          widthStart,
+          widthEnd,
+          widthDiff,
+        });
+      })}
+    </div>
+  );
+}
+
+interface RevealingTextItemProps {
+  scrollYProgress?: MotionValue<number>;
+  widthStart?: number;
+  widthEnd?: number;
+  widthDiff?: number;
+  index: number;
+  children: string;
+}
+
+export function RevealingTextItem({
+  scrollYProgress,
+  widthStart,
+  widthEnd,
+  widthDiff,
+  index,
+  children,
+}: RevealingTextItemProps) {
+  const width = useTransform(
+    scrollYProgress!,
+    [widthStart!, widthEnd! + widthDiff! * index],
+    ["100%", "0%"]
+  );
+  return (
+    <div className="relative">
+      <p>{children}</p>
+      <motion.div
+        style={{ width }}
+        className="mask absolute top-0 right-0 bg-black opacity-60 h-[calc(100%+1rem)] w-full"
+      ></motion.div>
+    </div>
+  );
+}
