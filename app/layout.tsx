91e397adc38c91f91bd94ab3bc8dc83224ae6ad2
Joy Samaddar <joysamaddar123@gmail.com>
2023-10-25 03:36:12 +0530

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Joy's Portfolio",
   description: "Making pixel-perfect UIs and robust backends.",
+  openGraph: {
+    type: "website",
+    siteName: "Joy's Portfolio",
+    title: "Joy's Portfolio",
+    description: "Making pixel-perfect UIs and robust backends.",
+    images: [
+      {
+        url: "https://0xjoy.tech/logo.png",
+      },
+    ],
+  },
+  keywords: [
+    "portfolio",
+    "joy",
+    "samaddar",
+    "joy samaddar",
+    "frontend",
+    "react",
+    "nextjs",
+    "developer",
+    "backend",
+  ],
+  themeColor: "#0d0d0d",
 };
 
 export default function RootLayout({
